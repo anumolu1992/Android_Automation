55cf25725e1679b04e3196c1b43d7d021aba21af
b0a00je <Bharath.anumolu@walmartlabs.com>
2022-05-09 21:27:33 -0700

    Reading input from data provider

--- src/test/java/RestCountries_Validation.java
+++ src/test/java/RestCountries_Validation.java
@@ -2,42 +2,65 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-
 public class RestCountries_Validation {
+    String BASE_URI = "https://restcountries.com/v3.1/";
+    String CITY_NAME_PATH = "alpha/";
+    String CAPITAL_PATH = "capital/";
+    String CITY_CODE_PATH = "?codes=";
+
+    @DataProvider(name = "city-name-provider")
+    public Object[][] dpMethodCityProvider(){
+        return new Object[][] {{"per"}};
+    }
+
+    @DataProvider(name = "capital-provider")
+    public Object[][] dpMethodCapitalProvider(){
+        return new Object[][] {{"lima"}};
+    }
+
+    @DataProvider(name = "invalid-city-provider")
+    public Object[][] dpMethodInvalidCityProvider(){
+        return new Object[][] {{"76353839"}};
+    }
+    @DataProvider(name = "city-code-provider")
+    public Object[][] dpMethodCityCodeProvider(){
+        return new Object[][] {{"col","pe","at"}};
+    }
 
     /*Assert response code equals to success (200) */
-    @Test
-    void test_01(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/alpha/per");
+    @Test (dataProvider = "city-name-provider")
+    void test_01(String cityName){
+        Response response = RestAssured.get(BASE_URI+CITY_NAME_PATH+cityName);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
     }
 
     /*Assert country capital contains expected value */
-    @Test
-    void test_02(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/alpha/per");
+    @Test (dataProvider = "city-name-provider")
+    void test_02(String cityName){
+        Response response = RestAssured.get(BASE_URI+CITY_NAME_PATH+cityName);
         ResponseBody body = response.getBody();
         String bodyAsString = body.asString();
         Assert.assertEquals(bodyAsString.contains("Lima"), true);
     }
 
     /*Assert response code not equals to 400 */
-    @Test
-    void test_03(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/capital/lima");
+    @Test (dataProvider = "invalid-city-provider")
+    void test_03(String cityName){
+        Response response = RestAssured.get(BASE_URI+CAPITAL_PATH+cityName);
         int statusCode = response.getStatusCode();
         Assert.assertNotEquals(statusCode, 400);
     }
 
     /*Assert response code equals to 404 with invalid path */
-    @Test
-    void test_04(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/capital/76353839");
+    @Test (dataProvider = "invalid-city-provider")
+    void test_04(String cityName){
+        Response response = RestAssured.get(BASE_URI+CITY_NAME_PATH+CAPITAL_PATH+cityName);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 404);
         ResponseBody body = response.getBody();
@@ -47,9 +70,9 @@ public class RestCountries_Validation {
 
 
     /*Assert response code equals to 404 with invalid path and expected data not present in response */
-    @Test
-    void test_05(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/capital/76353839");
+    @Test (dataProvider = "invalid-city-provider")
+    void test_05(String cityName){
+        Response response = RestAssured.get(BASE_URI+CAPITAL_PATH+cityName);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 404);
         ResponseBody body = response.getBody();
@@ -58,9 +81,9 @@ public class RestCountries_Validation {
     }
 
     /*Assert response code equals to 200 with country code and expected data present in response */
-    @Test
-    void test_06(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/alpha?codes=col,pe,at");
+    @Test (dataProvider = "city-code-provider")
+    void test_06(String code1, String code2 , String code3){
+        Response response = RestAssured.get(BASE_URI+CITY_NAME_PATH+CITY_CODE_PATH+code1+","+code2+","+code3);
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
         ResponseBody body = response.getBody();
@@ -69,17 +92,17 @@ public class RestCountries_Validation {
     }
 
     /*Assert country capital in response matches expected value */
-    @Test
-    void test_07(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/alpha/per");
+    @Test (dataProvider = "city-name-provider")
+    void test_07(String cityName){
+        Response response = RestAssured.get(BASE_URI+CITY_NAME_PATH+cityName);
          List output = response.jsonPath().getList("capital[0]");
         Assert.assertEquals(output.get(0), "Lima");
     }
 
     /* Log time needed to fetch the response from the backend */
-    @Test
-    public static void getResponseTime(){
-        Response response = RestAssured.get("https://restcountries.com/v3.1/alpha/per");
+    @Test (dataProvider = "city-name-provider")
+    public void test_08_getResponseTime(String cityName){
+        Response response = RestAssured.get(BASE_URI+CITY_NAME_PATH+cityName);
         System.out.println("The time taken to fetch the response is "+ response.timeIn(TimeUnit.MILLISECONDS) + " milliseconds");
     }
 
